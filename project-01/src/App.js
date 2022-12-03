1b5d13a49fc7f91e27b961022abd95cb8156e937
Pavankumar Kori <pavankumarkori1@gmail>
2022-12-03 14:30:06 +0530

    New Entry

---
+++ project-01/src/App.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+
+console.log(data)
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            // <Card
+            //     key={item.id}
+            //     item={item}
+            // />
+            <Card
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+                openSpots={item.openSpots}
+            />
+        )
+    })
+
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
